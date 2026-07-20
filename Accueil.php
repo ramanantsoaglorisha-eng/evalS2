dee4e1e0ca38d244052d49f7d4e9f6c9f6b97b69
didibug66 <rajaonahdiamondra66@gmail.com>
2026-07-20 14:05:04 +0300

    trucs

--- Accueil.php
+++ Accueil.php
@@ -7,10 +7,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="design/style.css">
     <title>accueil</title>
 </head>
 <body>
-    <h1>produit à vendre</h1>
+    
     <button><a href="Vendre.php" value="Aller vendre"></a></button>
+    <h1 class="title">produit à vendre</h1>
+    <table>
+        <tr>
+            <th>nom membre</th>
+            <th>nom du produit</th>
+            <th>nb</th>
+            <th>acheter</th>
+        </tr>
+        <?php foreach ($resultat as $donnee){?> 
+            <tr>
+                <td><?= $donnee['nom_membre']; ?></td>
+                <td><?= $donnee['nom_produit']; ?></td>
+                <td>quantité</td>
+                <td>acheté</td>
+            </tr>
+        <?php } ?>
+    </table>
 </body>
 </html>
