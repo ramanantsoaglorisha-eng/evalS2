b29754838bb7b75956790816904a53714588f70d
didibug66 <rajaonahdiamondra66@gmail.com>
2026-07-20 11:23:25 +0300

    login-1

---
+++ traitement.php
@@ -0,0 +1,14 @@
+<?php
+    include("fonctions.php");
+    $numero_etu =$_GET["etu"];
+    if (isset($numero_etu)){
+        $verifie_num = sign_in($numero_etu);
+        if($verifie_num == 0){
+            header("Location:inscription.php");
+        }
+        if($verifie_num > 0){
+            header("Location:Accueil.php");
+        }
+    }
+
+?>
