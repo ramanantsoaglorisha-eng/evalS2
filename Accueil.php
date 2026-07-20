7143f69df70be88e32215a8f35e768e5790b2a61
ramanantsoaglorisha-eng <ramanantsoaglorisha@gmail.com>
2026-07-20 13:17:45 +0300

    design

--- Accueil.php
+++ Accueil.php
@@ -7,10 +7,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="design/style.css">
     <title>accueil</title>
 </head>
 <body>
-    <h1>produit à vendre</h1>
-    
+    <h1 class="title">produit à vendre</h1>
+    <table>
+        <tr>
+            <th>nom membre</th>
+            <th>nom du produit</th>
+            <th>nb</th>
+            <th>acheter</th>
+        </tr>
+        <?php while($donnee = mysqli_fetch_assoc($resultat)){?> 
+            <tr>
+                <td><?= $donnee['nom_membre']; ?></td>
+                <td><?= $donnee['nom_produit']; ?></td>
+                <td>quantité</td>
+                <td>acheté</td>
+            </tr>
+        <?php } ?>
+    </table>
 </body>
 </html>
